f2803f843027ad7b56fa4c4f770d584a160fc031
madcoded <aocfreeman@gmail.com>
2020-10-11 21:05:31 +0530

    completed sticky-header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,13 @@
 import "../styles/styles.css";
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader'
 
+new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 new MobileMenu();
+
 
 if (module.hot) {
   module.hot.accept();
